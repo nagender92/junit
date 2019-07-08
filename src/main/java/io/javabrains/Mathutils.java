a08a95b17c817172ec29e69bccf1fd03d25c8014
Nagender Pamu <nagender.pamu@gmail.com>
2019-07-08 17:29:49 +0530

    Intial commit

---
+++ src/main/java/io/javabrains/Mathutils.java
@@ -0,0 +1,9 @@
+package io.javabrains;
+
+public class Mathutils {
+	
+	public int add(int a,int b) {
+		return a + b;
+	}
+	
+}
---
+++ src/test/java/io/javabrains/MathutilsTest.java
@@ -0,0 +1,16 @@
+package io.javabrains;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class MathutilsTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("test succeed");
+	}
+
+}
